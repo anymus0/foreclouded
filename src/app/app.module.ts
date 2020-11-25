6ebe4511908f8838768bb7f524b137eb3908bd16
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-11-25 11:42:02 +0100

    add bootstrap & some compinents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CityWeatherComponent } from './city-weather/city-weather.component';
+import { HourlyWeatherComponent } from './city-weather/hourly-weather/hourly-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityWeatherComponent,
+    HourlyWeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
