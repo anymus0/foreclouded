1a368c9193611cf94f4c8d66379a7f7323a6fea0
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-11-28 17:05:19 +0100

    add prod env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  geocodeAPI_URL: 'https://app.geocodeapi.io/api/v1',
+  openweatherAPI_URL: 'https://api.openweathermap.org/data/2.5',
+  geocodeAPI_KEY: '',
+  openweatherAPI_KEY: ''
 };
