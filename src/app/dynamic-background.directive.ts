4e9a3c7271eb472c12f67fdbe5ae7d1eb2a972f8
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-11-30 18:02:11 +0100

    change stock img url to 'https'

--- src/app/dynamic-background.directive.ts
+++ src/app/dynamic-background.directive.ts
@@ -11,7 +11,7 @@ export class DynamicBackgroundDirective implements OnInit, OnChanges {
 
   private setBackground(): void {
     if (this.bgURL === null) {
-      this.el.nativeElement.style.backgroundImage = `url("http://designmadeinjapan.com/site/wp-content/uploads/2015/07/tumblr_nr2569nqX01qze3hdo1_r2_500.gif")`;
+      this.el.nativeElement.style.backgroundImage = `url("https://designmadeinjapan.com/site/wp-content/uploads/2015/07/tumblr_nr2569nqX01qze3hdo1_r2_500.gif")`;
     } else {
       this.el.nativeElement.style.backgroundImage = `url("${this.bgURL}")`;
     }
